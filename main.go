e170a1cca13977415a207f14232b4c1e059e4e83
Matt Moore <mattomata@gmail.com>
2021-09-10 13:30:42 -0700

    Implement providers for Github and Google OIDC.
    These detect the presence of ambient OIDC sources (e.g. GKE workload identity, Github Actions OIDC) and will furnish tokens from an available provider as they are requested.

--- main.go
+++ main.go
@@ -5,8 +5,28 @@ SPDX-License-Identifier: Apache-2.0
 
 package main
 
-import "log"
+import (
+	"log"
+
+	"knative.dev/pkg/signals"
+
+	"github.com/mattmoor/oidc-magic/pkg/providers"
+
+	// These are the registered plugins
+	_ "github.com/mattmoor/oidc-magic/pkg/providers/github"
+	_ "github.com/mattmoor/oidc-magic/pkg/providers/google"
+)
 
 func main() {
-	log.Print("NYI")
+	ctx := signals.NewContext()
+
+	if providers.Enabled(ctx) {
+		tok, err := providers.Provide(ctx, "sigstore")
+		if err != nil {
+			log.Fatalf("Provide() = %v", err)
+		}
+		log.Printf("Providers are enabled: %v", tok)
+	} else {
+		log.Print("Providers are not enabled :(")
+	}
 }
---
+++ pkg/providers/doc.go
@@ -0,0 +1,8 @@
+/*
+Copyright 2021 Matt Moore
+SPDX-License-Identifier: Apache-2.0
+*/
+
+// Package providers defines the APIs for providers to detect their relevance
+// and register themselves to furnish OIDC tokens within a given environment.
+package providers
